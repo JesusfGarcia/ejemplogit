2ded7ca2b6d001a60fec5a37bbed389c5c13b2db
ernt <ernestn@ciencias.unam.mx>
2022-09-09 17:02:11 -0500

    Ernesto Commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 import Panchito from "../Views/jesus";
-
+import Ernesto from "../Views/ernesto";
 export const router = [
   {
     component: Panchito,
   },
+  {
+    component: Ernesto,
+  },
+
+
 ];
